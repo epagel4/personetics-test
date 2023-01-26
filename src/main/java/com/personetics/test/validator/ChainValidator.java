0be09362610582f2a0b6f355bb23c5e911acca9f
epagel <eduardosouzapagel@gmail.com>
2023-01-26 11:30:12 -0300

    update

--- src/main/java/com/personetics/test/validator/ChainValidator.java
+++ src/main/java/com/personetics/test/validator/ChainValidator.java
@@ -2,20 +2,12 @@ package com.personetics.test.validator;
 
 import com.personetics.test.Node;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 
 public class ChainValidator {
     public boolean validate(List<Node> nodes) {
-        boolean result = false;
-//        Arrays.stream(nodes.stream().toArray()).anyMatch(e -> validate())
-        for (int i = 0; i < nodes.size(); i++) {
-             if(Arrays.stream(nodes.stream().toArray()).anyMatch(o -> anyn))
-            }
-        System.out.println(result);
-        return result;
+        if(nodes.contains(List.of(nodes))){
+            return true;
+        } else return false;
     }
-
 }
